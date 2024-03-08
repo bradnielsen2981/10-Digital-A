3b85cc426f5e216e62a591ac05fd30973a278f83
Brad Nielsen <brad_nielsen29@hotmail.com>
2024-03-08 12:36:30 +1000

    commit

--- tictactoe.py
+++ tictactoe.py
@@ -5,9 +5,31 @@ turtle_object.speed(0)
 startx = -150
 starty = 150 
 
+# draw circle
+def circle(length, column, row):
+    turtle_object.goto(startx + column*length + 50, starty - row*length - 100)
+    turtle_object.pendown()
+    turtle_object.pencolor("green")
+    turtle_object.pensize(5)
+    turtle_object.circle(length/2)
+    turtle_object.penup()
+    return
+
+#cross function and test it
+def cross(length, column, row):
+    turtle_object.setheading(0)
+    turtle_object.goto(startx + column*length, starty - row*length)
+    turtle_object.pendown()
+    turtle_object.pencolor("red")
+    turtle_object.pensize(5)
+    turtle_object.right(45)
+    turtle_object.forward(120)
+    turtle_object.penup()
+    return
 
 # draw a square
 def square(length, column, row):
+    turtle_object.pensize(2)
     turtle_object.goto(startx + column*length, starty - row*length)
     turtle_object.pencolor("white")
     turtle_object.pendown()
@@ -16,21 +38,50 @@ def square(length, column, row):
         turtle_object.right(90)
     turtle_object.penup()
 
-                #row 0       #row 1        #row 2
-grid = [['_','_','_'],['_','_','_'],['_','_','_']]
+        #row 0 #row 1 #row 2
+grid = [['_','_','_'],
+        ['_','_','_'],
+        ['_','_','_']]
 
 for row in range(3):
     for column in range(3):
         square(100, column, row)
 
-input()
 
-# draw a cross
+turn = 1
+while True:
+    move = input("Please type your column and your row separated by a space: ")
+    if move == 'exit':
+        break
+    move = move.split()
+    column = int(move[0]) - 1 #1
+    row = int(move[1]) - 1 #1
+    if grid[row][column] != 0:
+        print("square is already taken")
+        continue #skip the rest of the loop
 
-# draw a circle
+    #turn
+    grid[row][column] = turn
 
-# draw a grid
-# player1 chooses a coordinate and places a cross
-# player2 chooses a coordinate and places a nought
-# repeat the above until one player has three in a row (horizonatally, vertically or diagonally)
+    win = False
+    #horizontal victory
+    for row in grid:
+        if row[0] == row[1] == row[2]:
+            print("Player", turn, "wins!")
+            win = True
+    #vertical victory
+    for c in range(3):
+        if grid[0][c] == grid[1][c] == grid[2][c]:
+            print("Player", turn, "wins!")
+            win = True
+    if win:
+        break
+    if turn == 1:
+        cross(100, column, row) #draw a circle
+        turn = 2
+    elif turn == 2:
+        circle(100, column, row)
+        turn = 1
+    
+
 
